ceaac10bd269f8842cf4209bd5b473185f5a8b58
Kai Curry <kai@cape.io>
2019-01-09 20:38:28 -0500

    eslint

--- src/utils.js
+++ src/utils.js
@@ -1,19 +1,25 @@
-const { add, curry, flow, get, getOr, identity, overArgs, set } = require('lodash/fp')
-const { doProp, setField, setWith } = require('cape-lodash')
+const {
+  curry, flow, getOr, overArgs, set,
+} = require('lodash/fp')
+const { doProp, setField } = require('cape-lodash')
 const { addFetchArgs } = require('./providers')
 
 const INDEX_FILE = 'index'
 const DEFAULT_EXT = '.html'
 
-const mergeFieldsWith = curry((withId, transformer, item) =>
-  ({ ...item, ...doProp(transformer, withId)(item) }))
+const mergeFieldsWith = curry((withId, transformer, item) => ({
+  ...item,
+  ...doProp(transformer, withId)(item),
+}))
 
 // Get subdomain value.
 function parseUrl(req) {
   const { headers, url } = req
   const { hostname, pathname } = new URL(url)
   const subdomain = hostname.split('.')[0]
-  return { headers, hostname, pathname, subdomain }
+  return {
+    headers, hostname, pathname, subdomain,
+  }
 }
 
 // Decide if route needs an index file
@@ -33,8 +39,7 @@ const addExt = pathname => (needsExt(pathname) ? pathname.concat(DEFAULT_EXT) : 
 const getPath = pathname => addExt(addIndex(pathname))
 
 // Combine bucketPath for entire domain with the specific path requested.
-const getFilePath = ({ container, pathname }) =>
-  `/${container}${getPath(pathname)}`
+const getFilePath = ({ container, pathname }) => `/${container}${getPath(pathname)}`
 
 function adjustContainer(item) {
   const { container, isDefault, subdomain } = item
